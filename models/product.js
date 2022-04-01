ee5ada5cf623c36735d875299754fa8bee2a4839
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-04-01 02:40:28 +0100

    added one

--- models/product.js
+++ models/product.js
@@ -1,27 +1,44 @@
-const Sequelize = require('sequelize');
+const {getDb}=requre("../util/database.js")
 
-const sequelize = require('../util/database');
 
-const Product = sequelize.define('product', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  title: Sequelize.STRING,
-  price: {
-    type: Sequelize.DOUBLE,
-    allowNull: false
-  },
-  imageUrl: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  description: {
-    type: Sequelize.STRING,
-    allowNull: false
+class Product{
+  constructor(title,price,description,imageUrl ){
+     this.title=this.title,
+     this.price=price,
+     this.description=this.description,
+     this.imageUrl=this.imageUrl
   }
-});
+  save(){
+     const db=getDb()
+     db.connection('products').RecordsDB.insertOne({
+        this
+     }).then(()=>{
+         
+     })   
+  }
+}
+
+
 
 module.exports = Product;
+// const Poduct = sequelize.define('product', {
+//   id: {
+//     type: Sequelize.INTEGER,
+//     autoIncrement: true,
+//     allowNull: false,
+//     primaryKey: true 
+//   },
+//   title: Sequelize.STRING,
+//   price: {
+//     type: Sequelize.DOUBLE,
+//     allowNull: false
+//   },
+//   imageUrl: {
+//     type: Sequelize.STRING,
+//     allowNull: false
+//   },
+//   description: {
+//     type: Sequelize.STRING,
+//     allowNull: false
+//   }
+// });
--- util/database.js
+++ util/database.js
@@ -2,12 +2,26 @@
 const { MongoClient, } = require('mongodb');
 const uri = "mongodb+srv://udemy-learn:ayomiku123@cluster0.xcjno.mongodb.net/udemyFirstDatabase?retryWrites=true&w=majority";
 
+let _db;
 
-MongoClient.connect(uri).then(data=>{
-  console.log("connected")
-})
-.catch(err=>{
-  console.log("failed")
-})
+const connectDb=(callback)=>{
+  return MongoClient.connect(uri)
+  .then(data=>{
+    console.log("connected")
+     _db=data.db 
+     callback()
+  })
+  .catch(err=>{ 
+    throw err
+  })
+}
+const getDb=()=>{
+  if(_db){
+    return _db
+  }
+  throw "No database found"
+}
+module.exports={connectDb,getDb}
+
 
 
